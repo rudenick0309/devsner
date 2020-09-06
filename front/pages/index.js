367e71b3b68d20c39841d90ed44ce05b6364807e
rudenick0309 <rudenick0309@gmail.com>
2020-09-06 17:23:53 +0900

    Feat: added grid system in all pages

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,44 +1,20 @@
 import * as React from "react";
-import { useState, useCallback } from 'react';
-import { useObserver, useLocalStore } from 'mobx-react'
-import { inputStore } from "../store/store";
-import { action } from 'mobx';
+import {useState, useCallback} from "react";
+import {useObserver, useLocalStore} from "mobx-react";
+import {inputStore} from "../store/store";
+import {action} from "mobx";
+import {Row, Col} from "antd";
+import AppLayout from "../components/AppLayout";
+
 
 const Home = () => {
-  const state = useLocalStore(() => ({
-    value: '',
-    onChangeTarget: action((a) => {
-      state.value = a.target.value;
-    }),
-    areaValue: '',
-    onChangeArea: action((a) => {
-      state.areaValue = a.target.value;
-    })
-  }))
 
-  const onSubmit = useCallback((e) => {
-    e.preventDefault()
-    console.log('in onsubmit, value: ', state.value, '/', state.areaValue);
-    inputStore.inputGo({value: state.value, areaValue:state.areaValue});
-    // inputStore.inputGo({state.value, state.areaValue});
-  }, [])
+  return (
+    <AppLayout>
+      <div>home test</div>
+    </AppLayout>
 
-
-  return useObserver(()=>(
-    <>
-      <div>
-        <form onSubmit={onSubmit}>
-          <input value={state.value} onChange={state.onChangeTarget}/>
-          <textarea value={state.areaValue} onChange={state.onChangeArea} />
-          {/*<input type="submit" value="Submit" />*/}
-          <button type={'submit'}/>
-        </form>
-
-        {/*{inputStore.render && <div>{inputStore.render}</div>}*/}
-
-      </div>
-    </>
-  ));
+  );
 };
 
 export default Home;
