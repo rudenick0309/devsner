25583010807a7867f5331d90c7fc3e42f0fdda23
rudenick0309 <rudenick0309@gmail.com>
2020-09-06 20:51:05 +0900

    Add: react page in error category, there are no function in now.

---
+++ front/pages/errors/react.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import AppLayout from "../../components/AppLayout";
+
+const errorReact = () => {
+  return (
+    <AppLayout>
+      <form>
+
+      </form>
+    </AppLayout>
+  );
+};
+
+export default errorReact;
