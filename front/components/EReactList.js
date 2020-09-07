e017f764ec79a981be3b6bed264c2336ebd80a86
rudenick0309 <rudenick0309@gmail.com>
2020-09-07 13:32:58 +0900

    Add: errorsReact route for Create, Read - but, Read function has a problem that when data which called the result of the ajax api is 'Proxy'

---
+++ front/components/EReactList.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const EReactList = (props) => {
+  console.log('in EReactList, props', props)
+
+  return (
+    <>
+      <div>테스트</div>
+      </>
+  )
+}
+
+export default EReactList;
