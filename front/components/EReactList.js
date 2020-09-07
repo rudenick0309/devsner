97b6c43f9d378adef1724b666c41140950645ff2
rudenick0309 <rudenick0309@gmail.com>
2020-09-08 00:44:36 +0900

    Feat: failed to delievery to Post component what the thing that the getServerSideProps function returns a value. - undefined

--- front/components/EReactList.js
+++ front/components/EReactList.js
@@ -1,13 +1,21 @@
 import * as React from "react";
 import ReactHtmlParser from "react-html-parser";
+import Link from "next/link";
 
 const EReactList = ({data}) => {
   console.log("in EReactList, props", data);
-  const {title, content, createdAt} = data;
+  const {id, title, content, createdAt} = data;
 
   return (
     <>
-      <div>{title}</div>
+      <div>
+        <Link href={`/posts/[id]`} as={`/posts/${id}`}>
+          <a>
+            {/*<img src={`/static/images/${feed.slug}.png`} />*/}
+            <div>{title}</div>
+          </a>
+        </Link>
+      </div>
     </>
   );
 };
