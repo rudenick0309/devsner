c4e236f450cadc89f14c2e672c0142aaf045e857
rudenick0309 <rudenick0309@gmail.com>
2020-09-07 13:32:34 +0900

    Add: errorsReact route for Create, Read - but, Read function has a problem that when data which called the result of the ajax api is 'Proxy'

--- front/pages/errors/react.js
+++ front/pages/errors/react.js
@@ -1,16 +1,32 @@
+
 import * as React from "react";
+import { useEffect, useState } from 'react';
 import AppLayout from "../../components/AppLayout";
+import {useObserver, useLocalStore} from 'mobx-react'
+import { action } from 'mobx';
+import {errorsReact} from "../../store/errorsReact";
 import dynamic from 'next/dynamic'
+import EReactList from "../../components/EReactList";
 const Editor = dynamic(() => import('../../components/Editor'), {
   ssr:false
 })
 
 const errorReact = () => {
-  return (
+
+  useEffect(() => {
+    errorsReact.errorsReactR()
+  }, [])
+
+  console.log('in errorsReact, render; ', errorsReact.render);
+
+  return useObserver(() => (
     <AppLayout>
       <Editor/>
+      {/*{errorsReact.render && errorsReact.render.map((el) => {*/}
+      {/*  return <EReactList data={el} />*/}
+      {/*})}*/}
     </AppLayout>
-  );
+  ));
 };
 
 export default errorReact;
