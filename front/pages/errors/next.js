b4c3ec665d0d304cac1623ffaaec408ce58db3b0
rudenick0309 <rudenick0309@gmail.com>
2020-09-09 17:57:02 +0900

    Feat: 1. added Link function to 'study-logs', 2. deleted toggle in SubMenu of antd, 3. ejected Edit component to header menu and deleted this in the pages of react, next and js, 4. added 'category' to Edit component

---
+++ front/pages/errors/next.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import {useEffect, useState} from "react";
+import AppLayout from "../../components/AppLayout";
+import {useObserver, useLocalStore} from "mobx-react";
+import {action} from "mobx";
+import {errorsReact} from "../../store/errorsReact";
+import dynamic from "next/dynamic";
+import EReactList from "../../components/EReactList";
+
+const Editor = dynamic(() => import("../../components/Editor"), {
+  ssr: false
+});
+import {toJS} from "mobx";
+
+const eNext = () => {
+
+  useEffect(() => {
+    errorsReact.errorsReactR("2");
+  }, []);
+
+  return useObserver(() => (
+    <AppLayout>
+      {/*<Editor/>*/}
+      {toJS(errorsReact.render) && toJS(errorsReact.render).reverse().map((el) => {
+        return <EReactList data={el}/>;
+      })}
+    </AppLayout>
+  ));
+};
+
+export default eNext;
