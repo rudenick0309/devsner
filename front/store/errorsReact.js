a1e2c9f346f7de45773ef56c8912a8a9f502dc98
rudenick0309 <rudenick0309@gmail.com>
2020-09-09 17:56:50 +0900

    Feat: 1. added Link function to 'study-logs', 2. deleted toggle in SubMenu of antd, 3. ejected Edit component to header menu and deleted this in the pages of react, next and js, 4. added 'category' to Edit component

--- front/store/errorsReact.js
+++ front/store/errorsReact.js
@@ -5,11 +5,13 @@ const errorsReact = observable({
   render: null,
 
   errorsReactC: action(async (data) => {
+    console.log('mobx, errorsReactC, data ; ', data)
     const result = await axios.post("http://localhost:3001/errors/react", {data});
   }),
 
-  errorsReactR: action(async () => {
-    const result = await axios.get("http://localhost:3001/errors/react");
+  errorsReactR: action(async (data) => {
+    console.log('mobx errorsReactR data;', data);
+    const result = await axios.get(`http://localhost:3001/errors/react?key=${data}`);
     errorsReact.render = result.data;
   })
 });
