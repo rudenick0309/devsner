06f853af8bd419c47b572d7174fc8b725be32284
rudenick0309 <rudenick0309@gmail.com>
2020-09-10 00:59:14 +0900

    Feat: changed board lib to toastUI - image, added url, movie functions are very well!

--- front/components/Editor.js
+++ front/components/Editor.js
@@ -1,67 +1,110 @@
-import React, {useCallback} from "react";
-import CKEditor from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import React, {useCallback, useRef, useState, useEffect, Component} from "react";
+
+import {Editor} from "@toast-ui/react-editor";
+
 import ReactHtmlParser from "react-html-parser";
 import {editPost} from "../store/editPost";
 import {useLocalStore, useObserver} from "mobx-react";
 import {action} from "mobx";
+import {dynamicPost} from "../store/dynamicPost";
 
-const Editor = () => {
-  const state = useLocalStore(() => ({
-    title: "",
-    onChangeTitle: action((e) => {
-      state.title = e.target.value;
-    }),
 
-    value: "",
-    onChangeContent: action((e, editor) => {
-      state.value = editor.getData();
-    }),
+class EditArticle extends Component {
+  editorRef = React.createRef();
+  titleRef = React.createRef();
+  categoryRef = React.createRef();
 
-    category: "1",
-    onChangeCategory: action((e) => {
-      state.category = e.target.value;
-    })
-  }));
+  constructor() {
+    super();
+    this.state = {
+      content: "",
+      title: "",
+      category: "1",
+    };
+  }
 
-  const onSubmit = useCallback((e) => {
+  onSubmit = (e) => {
     e.preventDefault();
-    editPost.postC({category: state.category, title: state.title, content: state.value});
-    state.title = "";
-    state.value = "";
-  }, [state.title, state.value]);
+    if (this.categoryRef.current === null) {
+      this.categoryRef.current = '1'
+    }
+    this.setState({
+      content: this.editorRef.current.getInstance().getHtml(),
+      title: this.titleRef.current,
+      category: this.categoryRef.current,
+    });
+  };
 
-  return useObserver(() => (
-    <>
-      <form onSubmit={onSubmit}>
-        <input type={"text"} value={state.title} onChange={state.onChangeTitle}/>
-        <select value={state.category} onChange={state.onChangeCategory}>
-          <option value="1">errors react</option>
-          <option value="2">errors next</option>
-          <option value="3">errors js</option>
-          <option value="5">study logs react</option>
-          <option value="6">study logs next</option>
-          <option value="7">study logs js</option>
-        </select>
-        <CKEditor
-          data={state.value}
-          onInit={editor =>
-            console.log("Editor is ready to use!", editor)
-          }
-          onChange={state.onChangeContent}
-          config={{
-            ckfinder: {
-              uploadUrl: "/upload/images"
-            }
-          }}
-          editor={ClassicEditor}
-        />
-        <input type={"submit"} title={"submit"}/>
-      </form>
+  onChangeTitle = (e) => {
+    this.titleRef.current = e.target.value;
+  };
 
-      {state.value && <div>{ReactHtmlParser(state.value)}</div>}
-    </>
-  ));
-};
+  onChangeCategory = (e) => {
+    this.categoryRef.current = e.target.value;
+  };
 
-export default Editor;
+  componentDidUpdate() {
+    editPost.postC(this.state);
+  }
+
+
+  render() {
+    console.log("f editor content; ", this.state.content);
+    return (
+      <>
+        <form onSubmit={this.onSubmit}>
+          <input type={"text"} value={this.title} onChange={this.onChangeTitle}/>
+
+          <select value={this.category} onChange={this.onChangeCategory}>
+            <option value="1">errors react</option>
+            <option value="2">errors next</option>
+            <option value="3">errors js</option>
+            <option value="5">study logs react</option>
+            <option value="6">study logs next</option>
+            <option value="7">study logs js</option>
+          </select>
+          <Editor
+            previewStyle="vertical"
+            height="300px"
+            initialEditType="wysiwyg"
+            placeholder="글쓰기"
+            ref={this.editorRef}
+          />
+          <input type={"submit"} title={"submit"}/>
+        </form>
+      </>
+    );
+  }
+}
+
+export default EditArticle;
+
+
+//
+// <form onSubmit={onSubmit}>
+//   <input type={"text"} value={state.title} onChange={state.onChangeTitle}/>
+//   <select value={state.category} onChange={state.onChangeCategory}>
+//     <option value="1">errors react</option>
+//     <option value="2">errors next</option>
+//     <option value="3">errors js</option>
+//     <option value="5">study logs react</option>
+//     <option value="6">study logs next</option>
+//     <option value="7">study logs js</option>
+//   </select>
+//   <CKEditor
+//     data={state.value}
+//     onInit={editor =>
+//       console.log("Editor is ready to use!", editor)
+//     }
+//     onChange={state.onChangeContent}
+//     config={{
+//       ckfinder: {
+//         uploadUrl: "/upload/images"
+//       }
+//     }}
+//     editor={ClassicEditor}
+//   />
+//   <input type={"submit"} title={"submit"}/>
+// </form>
+//
+// {state.value && <div>{ReactHtmlParser(state.value)}</div>}
