fb32879de44e802248036e4296699981e19d635e
rudenick0309 <rudenick0309@gmail.com>
2020-09-09 13:09:20 +0900

    Feat: dynamic route - but, the value rendered was existing. I want to new a value. For solving this problem, I have tried to setTimeout function, mobx state

--- front/components/EReactList.js
+++ front/components/EReactList.js
@@ -1,18 +1,31 @@
 import * as React from "react";
+import { useState, useEffect, useCallback } from 'react';
 import ReactHtmlParser from "react-html-parser";
 import Link from "next/link";
+import {dynamicPost} from "../store/dynamicPost";
 
 const EReactList = ({data}) => {
-  console.log("in EReactList, props", data);
+  // console.log("in EReactList, props", data);
   const {id, title, content, createdAt} = data;
+  const [post, setPost] = useState('')
 
+  const onClickPost = useCallback(() => {
+    console.log('EReactList 컴포넌트입니다 - 1')
+    dynamicPost.eReactPostR(id);
+    // console.log('EReactList 컴포넌트입니다 - 1')
+    setTimeout(() => {
+
+    },3000)
+  },[])
+
+  // console.log('in EReactList, post;',post);
   return (
     <>
       <div>
-        <Link href={`/posts/[id]`} as={`/posts/${id}`}>
+        <Link href={`/posts/[id]`} as={`/posts/${id}`} >
           <a>
             {/*<img src={`/static/images/${feed.slug}.png`} />*/}
-            <div>{title}</div>
+            <div onClick={onClickPost}>{title}</div>
           </a>
         </Link>
       </div>
