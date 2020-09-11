81aaee68d0ade03165841b6f58ae382f972166f1
rudenick0309 <rudenick0309@gmail.com>
2020-09-11 22:18:52 +0900

    Feat: added login and signup router, including 'passport lib'. In passport, just 'local' version had been coded, not the 'kakao' and 'facebook' and so on. Tomorrow, logout router will had coded.

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,21 @@
 const express =require('express')
-
 const router = express.Router();
+const ErrorsReact = require('../models/errorsReact')
+const User = require('../models/user')
 
-router.get('/', (req,res,next) => {
-  res.send('route executes')
+router.get('/', async (req,res,next) => {
+  console.log('b, routes index, / executes', req);
+  try {
+    const user = await User.findOne({
+      attributes:['email'],
+    })
+    console.log('b routex index, post', user);
+    res.json(user)
+  } catch(error) {
+    console.error(error);
+    next(error);
+  }
+
 })
 
 module.exports = router;
