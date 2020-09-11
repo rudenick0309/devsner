41e8859e1dafe838e1fc3eca0f7451a7ccbd8e4a
rudenick0309 <rudenick0309@gmail.com>
2020-09-11 12:06:05 +0900

    Feat: added 'limit' option for image upload in app.js in backend category

--- back/app.js
+++ back/app.js
@@ -29,8 +29,10 @@ sequelize.sync({
 
 
 app.use(morgan('dev'))
-app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.json({
+  limit:'50mb'
+}));
+app.use(express.urlencoded({limit:'50mb',extended:true}))
 app.use(cookieParser(process.env.COOKIE_SECRET))
 
 app.use(cors({
